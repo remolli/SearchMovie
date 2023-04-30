c0de42cf0b4ba37a52ea3c7984f429a938b15b62
Evandru <evandro_remolli.t@outlook.com>
2023-04-30 16:13:23 -0300

    Controllers

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchMovie.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
