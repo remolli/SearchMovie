fdda94cede8cf9b1228d203c86978d33d7689b2c
Evandru <evandro_remolli.t@outlook.com>
2023-04-29 23:15:15 -0300

    SearchMovie

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SearchMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
