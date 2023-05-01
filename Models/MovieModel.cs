80025396c450af29c0f9376abdd67d82677d4476
Evandru <evandro_remolli.t@outlook.com>
2023-05-01 15:46:28 -0300

    Update MovieModel

--- Models/MovieModel.cs
+++ Models/MovieModel.cs
@@ -2,6 +2,26 @@
 {
     public class MovieModel
     {
+        // ------> GET SEARCH MOVIE
+
+        public class SearchResponse
+        {
+            public List<MovieBySearch> Search { get; set; }
+            public string totalResults { get; set; }
+            public string Response { get; set; }
+        }
+
+        public class MovieBySearch
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string imdbID { get; set; }
+            public string Type { get; set; }
+            public string Poster { get; set; }
+        }
+
+        // ----> GET MOVIE
+
         public class Rating
         {
             public string Source { get; set; }
