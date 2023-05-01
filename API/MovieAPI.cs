03cf659e73350a7457d2d98aa027418cf7c50e92
Evandru <evandro_remolli.t@outlook.com>
2023-05-01 15:53:47 -0300

    Update MovieAPI and GetSearchResponse

--- API/MovieAPI.cs
+++ API/MovieAPI.cs
@@ -13,18 +13,26 @@ namespace SearchMovie.API
             httpClient = new HttpClient();
             this.apiKey = apiKey;
         }
+
         public async Task<string> GetJsonString(string link)
         {
-            HttpClient httpClient = new HttpClient();
             var response = await httpClient.GetAsync(link + apiKey);
             var jsonString = await response.Content.ReadAsStringAsync();
             return jsonString;
         }
-        public async Task<MovieModel> GetMovie(string title)
+
+        public async Task<MovieModel.Movie> GetMovie(string title)
         {
             var jsonString = await GetJsonString($"https://www.omdbapi.com/?t={title}&apikey=");
-            MovieModel movie = JsonConvert.DeserializeObject<MovieModel>(jsonString);
+            MovieModel.Movie movie = JsonConvert.DeserializeObject<MovieModel.Movie>(jsonString);
             return movie;
+        }
+
+        public async Task<MovieModel.SearchResponse> GetSearchResponse(string search)
+        {
+            var jsonString = await GetJsonString($"https://www.omdbapi.com/?s={search}&apikey=");
+            MovieModel.SearchResponse searchResponse = JsonConvert.DeserializeObject<MovieModel.SearchResponse>(jsonString);
+            return searchResponse;
         }
     }
 }
