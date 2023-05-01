d3f2c3399d48020b8c4c169d227f402e9b88db5a
Evandru <evandro_remolli.t@outlook.com>
2023-05-01 15:40:10 -0300

    API

---
+++ API/MovieAPI.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SearchMovie.Models;
+
+namespace SearchMovie.API
+{
+    public class MovieAPI
+    {
+        HttpClient httpClient;
+        string apiKey;
+        
+        public MovieAPI(string apiKey)
+        {
+            httpClient = new HttpClient();
+            this.apiKey = apiKey;
+        }
+        public async Task<string> GetJsonString(string link)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(link + apiKey);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return jsonString;
+        }
+        
+    }
+}
